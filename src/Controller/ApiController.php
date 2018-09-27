34fa08398d8ab71e8779a08917c003801fe8ad76
Ju/TroisYaourts <julien@troisyaourts.com>
2018-09-27 15:40:41 +0200

    annotation + ctrl

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -17,4 +17,80 @@ class ApiController extends Controller{
     return $this->json(["status" => "ok"]);
 
   }
+
+  /**
+   * @Route("/bol", name="bol", methods={"GET"})
+   **/
+  public function bol(Request $request):Response{
+    $bol = [
+      "id" => "123456-12345678-34567";
+      "items" => []
+    ];
+    return $this->json($bol, 200);
+  }
+
+  /**
+   * @Route("/bol", name="bol", methods={"POST"})
+   **/
+  public function create(Request $request):Response{
+    $bol = [
+      "id" => "123456-12345678-34567";
+      "items" => []
+    ];
+    return $this->json($bol, 201);
+  }
+
+  /**
+   * @Route("/bol", name="bol", methods={"PUT"})
+   **/
+  public function addItem(Request $request):Response{
+    $item = $request->request->get("item");
+    $bol = [
+      "id" => "123456-12345678-34567";
+      "items" => []
+    ];
+
+    $bol["items"][] = $item;
+    return $this->json($bol, 201);
+  }
+
+  /**
+   * @Route("/bol", name="bol", methods={"DELETE"})
+   **/
+  public function clear(Request $request):Response{
+    $bol = [
+      "id" => "123456-12345678-34567";
+      "items" => ["pizza", "pâtes"]
+    ];
+
+    $bol["items"] = [];
+    return $this->json($bol, 201);
+  }
+
+  /**
+   * @Route("/bol/shake", name="bol", methods={"GET"})
+   **/
+  public function shake(Request $request):Response{
+    $bol = [
+      "id" => "123456-12345678-34567";
+      "items" => ["pizza", "pâtes"]
+    ];
+
+    shuffle($bol["items"]);
+    return $this->json($bol, 200);
+  }
+
+  /**
+   * @Route("/bol/draw", name="bol", methods={"GET"})
+   **/
+  public function draw(Request $request):Response{
+    $bol = [
+      "id" => "123456-12345678-34567";
+      "items" => ["pizza", "pâtes"]
+    ];
+
+    $item = array_pop($bol["items"]);
+    return $this->json($item, 201);
+  }
+
 }
